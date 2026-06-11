df76e7b058465a2118e9ae4981a5b007ba4db005
Pradeep-venkatachalam <pradeepxyp7@gmail.com>
2026-06-11 22:35:45 +0530

    jenkins update

--- AttendanceManagementSystem_fixed/src/test/java/org/example/attendancemanagementsystem/AttendanceManagementSystemApplicationTests.java
+++ AttendanceManagementSystem_fixed/src/test/java/org/example/attendancemanagementsystem/AttendanceManagementSystemApplicationTests.java
@@ -2,12 +2,13 @@ package org.example.attendancemanagementsystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AttendanceManagementSystemApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
